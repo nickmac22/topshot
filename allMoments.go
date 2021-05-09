5b006d6bdf58d5727f9d95965db6059fdd2583b2
nickmac22 <83890802+nickmac22@users.noreply.github.com>
2021-05-08 17:46:57 -0700

    Create allMoments.go

---
+++ allMoments.go
@@ -0,0 +1,43 @@
+package topshot
+import (
+	"context"
+	"fmt"
+	"github.com/onflow/flow-go-sdk/client"
+	"google.golang.org/grpc"
+)
+
+func handleErr(err error) {
+	if err != nil {
+		
+	}
+}
+func getAllMoments() {
+    // connect to flow
+    flowClient, err := client.New("access.mainnet.nodes.onflow.org:9000", grpc.WithInsecure())
+    handleErr(err)
+    err = flowClient.Ping(context.Background())
+    handleErr(err)
+	   
+    script := []byte(`
+
+	import TopShot from 0x0b2a3299cc857e29
+
+	// This script returns an array of all the plays 
+	// that have ever been created for Top Shot
+
+	// Returns: [TopShot.Play]
+	// array of all plays created for Topshot
+
+	pub fun main(): [TopShot.Play] {
+
+		return TopShot.getAllPlays()
+	}
+	`)
+	ctx := context.Background()
+    value, err := flowClient.ExecuteScriptAtLatestBlock(ctx, script, nil)
+    if err != nil {
+        panic("failed to execute script")
+    }
+
+    fmt.Println(value)
+}
